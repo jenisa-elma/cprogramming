224e2f2dbd2c3667e9457d2772538b47a5919ede
jenisa-elma <62924520+jenisa-elma@users.noreply.github.com>
2020-04-01 00:14:33 +0530

    First commit

---
+++ info.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Name:Jenisa Elma Benjamin\n");
+    printf("Department:Electronics and Communication Engineering\n");
+    printf("College:College of Engineering,Chengannur\n");
+    return 0;
+}
---
+++ learncoding.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+    printf("Welcome to learn to code");
+    return 0;
+}
