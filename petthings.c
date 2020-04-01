d6e93bd10dcf658438203059bce120b253febb87
jenisa-elma <62924520+jenisa-elma@users.noreply.github.com>
2020-04-01 22:03:22 +0530

    Second commit

---
+++ petthings.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    printf("Name of my pet=%s\n","Judy");
+    printf("Age=%d\n",11);
+    printf("Few favourite food=%s,%s,%s\n","meat","biscuits","milk");
+    return 0;
+}
---
+++ studentdata.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int age=16;
+    int class=11;
+    double marks=94.6;
+    char name[]="Greg";
+    char school[]="Mar Thoma Residential School";
+printf("NAME=%s\n",name);
+printf("SCHOOL=%s\n",school);
+printf("AGE=%d\n",age);
+printf("CLASS=%d\n",class);
+printf("MARKS=%f\n",marks);
+return 0;
+
+}
